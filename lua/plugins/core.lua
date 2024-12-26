140e34a3ffd3c78511bbcfc33bd26da39758fb3a
zanderhavgaard <zander@havgaard.dk>
2024-12-26 15:13:15 +0100

    add auto-save plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,12 +1,25 @@
 return {
-  {
-   "navarasu/onedark.nvim"
-  },
+	{
+		"navarasu/onedark.nvim",
+	},
 
-  {
-    "LazyVim/LazyVim", 
-    opts = {
-      colorscheme = "onedark"
-    }
-  }
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "onedark",
+		},
+	},
+
+	{
+		"okuuva/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({
+				trigger_events = { -- See :h events
+					immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+					defer_save = {}, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+					cancel_deferred_save = {}, -- vim events that cancel a pending deferred save
+				},
+			})
+		end,
+	},
 }
