c0a38e170942dd58fdb43cb5016c485f12adb393
zanderhavgaard <zander@havgaard.dk>
2025-02-02 19:12:43 +0100

    disable mason-nvim-dap to use system binaries instead

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -28,6 +28,7 @@ return {
   -- disable installing binaries from mason - use system binaries instead
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "williamboman/mason.nvim", enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 
   {
     "LazyVim/LazyVim",
