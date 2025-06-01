e7a63edf996d161ed4845167dba0683ae0d151e5
zanderhavgaard <zander@havgaard.dk>
2025-06-01 16:27:28 +0200

    update plugins

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -74,8 +74,10 @@ return {
     },
   },
 
+  -- # TODO: enable/disable these conditionally
+  -- For when running on NixOS
   -- disable installing binaries from mason - use system binaries instead
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim", enabled = false },
-  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+  -- { "williamboman/mason-lspconfig.nvim", enabled = false },
+  -- { "williamboman/mason.nvim", enabled = false },
+  -- { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 }
