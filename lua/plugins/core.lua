f73e243aed3bb80bb4fc1d48ff8ad9b15082441b
zanderhavgaard <zander@havgaard.dk>
2025-06-05 17:23:42 +0200

    update neorg config

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -42,12 +42,30 @@ return {
     },
   },
 
-  -- neovim~orgmode
+  -- neovim~orgmode~neorg
   {
     "nvim-neorg/neorg",
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = "*", -- Pin Neorg to the latest stable release
-    config = true,
+    ft = "norg", -- only load on norg files
+    lazy = false,
+    version = "*",
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      })
+      vim.wo.foldlevel = 99
+      -- vim.wo.conceallevel = 2
+    end,
   },
 
   -- show hidden files in snacks picker/explorer
