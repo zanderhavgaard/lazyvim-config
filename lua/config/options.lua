b2ea2cae4a28bec54929a3593da2784e64c49abe
zanderhavgaard <zander@havgaard.dk>
2026-01-13 17:38:49 +0100

    pin root_spec to cwd, udpate plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- don't try to change the root after opening neovim
+vim.g.root_spec = { "cwd" }
