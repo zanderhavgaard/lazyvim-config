c66d8769db2b058b2565bcde892db976fe167063
zanderhavgaard <zander@havgaard.dk>
2026-09-05 12:17:20 +0200

    use onedark

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -43,7 +43,7 @@ return {
     priority = 1000,
     config = function()
       require("onedark").setup({
-        style = "darker",
+        style = "dark",
         highlights = {
           -- defualt comments can be hard to read ... use a different color
           ["@comment"] = { fg = "#6A9FB5" },
@@ -64,7 +64,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "onedark",
     },
   },
 
